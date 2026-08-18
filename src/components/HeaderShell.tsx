512a3766d630a4bd82271c20d430d7453db431c1
arminellenouatin-droid <3.11184451e+08+arminellenouatin-droid@users.noreply.github.com>
2026-08-18 11:50:36 +0000

    Render desktop header during initial page load

--- src/components/HeaderShell.tsx
+++ src/components/HeaderShell.tsx
@@ -1,11 +1,7 @@
-"use client";
-
-import dynamic from "next/dynamic";
+import Header from "./Header";
 
 type HeaderUser = { id: string; nom?: string; prenom?: string; email?: string; role?: string; avatar?: string };
 
-const InteractiveHeader = dynamic(() => import("./Header"), { ssr: false });
-
 export default function HeaderShell({ user }: { user?: HeaderUser }) {
-  return <InteractiveHeader user={user} />;
+  return <Header user={user} />;
 }
